63245bea7a6d6e830aad361a4ee9e44e7aea5960
David Hale and Sarah Lindmar <david@>
2017-08-17 11:04:32 -0700

    add basic functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,7 @@ $(document).ready(function() {
     event.preventDefault();
     var userInput = $('#romanNum').val();
     var userNum = userInput.split('');
-    var parsed = parseUserNum(userNum);
-    alert(parsed);
+    toRoman(userNum);
   })
 })
 var result = '';
@@ -12,37 +11,36 @@ var matrix = [
   ['M', 'MM', 'MMM'],
   ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
   ['X', 'XX', 'XXX', 'XV', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
-  ['I', 'II', 'III', 'IV', 'VI', 'VII', 'VIII', 'IX']
+  ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
 ];
 
-function parseUserNum(arr) {
-  for (var i = 0; i < arr.length; i++) {
-    arr[i] = parseInt(arr[i]);
-  }
-  return arr;
+function toRoman(arr) {
+    if (arr.length === 4) {
+      result += matrix[0] [arr[0]-1];
+      if (arr[1] != '0') {
+        result += matrix[1] [arr[1]-1];
+      }
+      if (arr[2] != '0') {
+        result += matrix[2] [arr[2]-1];
+      }
+      if (arr[3] != '0') {
+        result += matrix[3] [arr[3]-1];
+      }
+    } else if (arr.length === 3) {
+      result += matrix[1] [arr[0]-1];
+      if (arr[1] != '0') {
+        result += matrix[2] [arr[1]-1];
+      }
+      if (arr[2] != '0') {
+        result += matrix[3] [arr[2]-1];
+      }
+    } else if (arr.length === 2) {
+      result += matrix[2] [arr[0]-1];
+      if (arr[1] != '0') {
+        result += matrix[3] [arr[1]-1];
+      }
+    } else if (arr.length === 1) {
+      result += matrix[3] [arr[0]-1];
+    }
+  alert(result);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var romans = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
-// var decimal = ['1000', '500', '100', '50', '10', '5', '1']
-// function numToSym(num) {
-//   for (var i=0; i < romans.length; i++){
-//     while (num >= decimal[i]) {
-//       num -= decimal[i];
-//       result += romans[i];
-//     }
-//   }
-//   alert(result);
-// }
